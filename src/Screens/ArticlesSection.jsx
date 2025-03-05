7483cbc8bcc744fdbe4e02681033890fae2fb76e
BrayanMen <brayanjmr880@gmail.com>
2025-03-05 01:10:02 -0300

    modal

--- src/Screens/ArticlesSection.jsx
+++ src/Screens/ArticlesSection.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import MainArticles from "../componets/MainArticles";
-import { X } from "lucide-react";
 import CardArticle from "../componets/CardArticle";
+import ArticlesModal from "../componets/ArticlesModal";
 
 const ArticlesSection = () => {
   const [selectedProject, setSelectedProject] = useState(null);
@@ -57,54 +57,7 @@ const ArticlesSection = () => {
     if (articles.length > 0) {
       setSelectedProject(articles[0]);
     }
-  }, []);
-
-  const ArticlesModal = ({ article, onClose }) => {
-    useEffect(() => {
-      const handleKeyDown = (e) => {
-        if (e.key === 'Escape') onClose();
-      };
-      window.addEventListener('keydown', handleKeyDown);
-      return () => window.removeEventListener('keydown', handleKeyDown);
-    }, [onClose]);
-
-    return (
-      <div className="mt-8 w-fit mx-auto">
-        <div className="bg-space-cadet text-white p-8">
-          <div className="relative flex justify-end mb-4">
-            <button
-              onClick={onClose}
-              className="absolute top-4 right-4 text-lavender-web hover:text-red-600 transition-colors"
-              aria-label="Close expanded view"
-            >
-              <X size={42} />
-            </button>
-          </div>
-          <div className="space-y-6 ">
-            <div className="bg-olivine text-space-cadet px-4 py-1 inline-block font-poppins">
-              ARTICULO
-            </div>
-            <h1 className="text-3xl font-light font-poppins">{article.title}</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-              <div className="relative">
-                <img
-                  src={article.image}
-                  alt={article.title}
-                  className="w-full h-4/6 object-cover"
-                />
-                <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-gradient-to-t from-space-cadet/50 to-transparent" />
-              </div>
-              <div className="space-y-4">
-                <p className="font-light leading-relaxed text-lavender-web font-poppins">
-                  {article.text}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  };
+  }, []);  
 
   return (
     <section className="relative">
